7e6b56df0ad6e6877b6dea790309082fc378df89
Muhammad Ibrahim Jan <ibrahimjan31898@gmail.com>
2024-10-12 07:08:19 -0700

    Add work to Github repo

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+export class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Hello React World</Text>
+      </View>
+    )
+  }
+}
+
+export default App
